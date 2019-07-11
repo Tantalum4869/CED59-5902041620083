a96bf9c48f21f7b4dbd077857423b68fa67e741f
tantalum4869 <tantalum590@gmail.com>
2019-07-11 20:26:31 +0700

    sended assignment3

---
+++ assignment3/practise.py
@@ -0,0 +1,16 @@
+###-- ASSIGNMENT3 11 07 62 --###
+
+#1
+my_list = [48, 17, 35]
+print(sum(my_list));
+
+#2
+my_tuble = (59, 20, 41, 62, 83)
+print("Single value in a tuple is ",my_tuble)
+
+#3
+set1 = {40,6,5,88,77,3,9,91,12}
+set2 = {5,16,77,15,9,69,25,12,48}
+print(set1.intersection(set2))
+
+##..5902041620083 CHAIYASIT PRAJONGJIT CED 4RA..##
