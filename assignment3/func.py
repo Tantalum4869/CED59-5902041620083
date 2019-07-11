bf41fe60bf9f810b314d99306c9d576d44fd1d49
ชัยสิทธิ์ ประจงจิตร <s5902041620083@Macmini-213-11.local>
2019-07-11 14:39:39 +0700

    add files for assignment3

---
+++ assignment3/func.py
@@ -0,0 +1,7 @@
+#1
+def return_42():
+  return 42:
+
+#2
+def my_method(num1, num2):
+  return num1*num2;
---
+++ assignment3/main.py
@@ -0,0 +1,7 @@
+from func import *
+
+#1
+print("Result of return_42() is",return_42());
+
+#2
+print("Result of 5 x 8 is",my_method(5, 8));
