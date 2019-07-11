69fa10996591467561e69bde67aa0cf22c52a5ce
tantalum4869 <tantalum590@gmail.com>
2019-07-11 19:35:44 +0700

    assignment2

---
+++ assignment2/variable1.py
@@ -0,0 +1,7 @@
+#1
+var = 5902041620083
+var1 = var
+var2 = var
+
+print("var1 = ",var1)
+print("var2 = ",var2)
---
+++ assignment2/variable2.py
@@ -0,0 +1,8 @@
+#2
+num1 = 8
+num2 = 2
+
+print("num1 = ",num1)
+print("num2 = ",num2)
+
+print(Result of 8 x 2 = ",num1 * num2)
