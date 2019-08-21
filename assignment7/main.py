8db3c4fce6680047918e330557b5f15c2897fb47
tantalum4869 <tantalum590@gmail.com>
2019-08-21 22:20:22 +0700

    Add Assignment7

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,16 +1,33 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
+from wtforms import StringField, PasswordField
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired, Length, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'any secret string'
+
+
+class SignupForm(FlaskForm):
+    name = StringField("first name", validators=[InputRequired()])
+    last = StringField("last name", validators=[InputRequired()])
+    email = StringField("E-mail", [InputRequired("Please enter your E-mail address."), Email("Please enter your E-mail again.")])
+    username = StringField("username", validators=[InputRequired()])
+    password = PasswordField("password", validators=[InputRequired(), Length(min=8, message="Please enter at least 8 characters")])
+
 
 @app.route('/')
 def member():
-    return render_template('test.html')
+    form = SignupForm()
+    return render_template('signup.html', form=form)
 
-@app.route('/', methods = ["post"])
-def index():
-    return 'Sign Up OK!'
-if __name__ == "__main__":
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = SignupForm()
+    if form.validate_on_submit():
+        return "You are already registered."
+    return render_template('signup.html', form=form)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
